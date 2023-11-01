d2f934b265a3e9b48540d779721cc56d461c4fec
Jean-Marie <jeanmariedeschamps25@gmail.com>
2023-11-01 20:02:24 +0100

    code source

---
+++ src/pages/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum et quas necessitatibus natus aut voluptas. Minima voluptatibus, exercitationem beatae soluta veritatis mollitia maxime error officiis adipisci, omnis, nostrum necessitatibus animi. Omnis fugiat numquam, placeat quo natus sed minus consequuntur et repellendus accusamus reiciendis nihil saepe enim? Accusamus fuga quam rem! Similique earum iste, autem distinctio iure excepturi fugiat enim quisquam neque dolorem eius voluptates vero ad veritatis impedit exercitationem a non suscipit recusandae id porro ex cum? Natus, assumenda repudiandae molestias autem optio neque quidem harum deleniti quae similique illo at architecto mollitia, commodi saepe iure perferendis voluptates nisi! Exercitationem.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat delectus dolores amet! Explicabo dolorem illum consequuntur perspiciatis! Perferendis officiis magnam reiciendis totam vel explicabo deleniti, incidunt ab optio sint assumenda nisi. Totam aperiam, pariatur iure, soluta tempora nobis hic, in dignissimos ex doloremque voluptatum aspernatur quos voluptate qui enim! Dolores, blanditiis et? Amet at, exercitationem optio hic dolores consequuntur facilis. Alias quia corrupti cumque tempora sunt similique, qui eum a officiis reprehenderit officia repellendus amet corporis culpa ut expedita facere? Sed vero ipsum fugit distinctio reiciendis libero nulla voluptates! Incidunt animi sapiente, praesentium quo minima sint libero similique distinctio maiores.</p>
+        </div>
+        
+    );
+};
+
+export default About;
